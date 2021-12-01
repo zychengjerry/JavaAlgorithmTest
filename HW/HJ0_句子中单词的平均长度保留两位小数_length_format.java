ecff3026d982c94d5b1422907b4c47ef9dfd9be5
Jerry Cheng <zychengjerry@gmail.com>
2021-12-01 11:16:17 +0800

    Update HJ0

---
+++ HW/HJ0_句子中单词的平均长度保留两位小数_length_format.java
@@ -0,0 +1,21 @@
+package HW;
+
+import java.util.Scanner;
+
+public class HJ0_句子中单词的平均长度保留两位小数_length_format {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        String[] words = str.split(" ");
+        double sum = 0;
+        double avg = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            sum += words[i].length();
+        }
+
+        avg = sum/words.length;
+        //System.out.println(avg);
+        System.out.println(String.format("%.2f",avg).toString());   //保留两位小数
+    }
+}
